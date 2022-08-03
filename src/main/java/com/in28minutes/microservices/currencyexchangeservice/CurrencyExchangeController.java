f6cd9226516891cbcdf8993fd85073910f6cd8d0
Max Gomes <maximiliano.pinto@klarna.com>
2022-08-03 20:40:50 +0200

    135

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,15 +1,18 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+    @Autowired
+    private Environment environment;
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchangeResponse getCurrencyExchange (@PathVariable String from, @PathVariable String to) {
-        return new CurrencyExchangeResponse(1, from, to, 65, "8000 instance-id");
+        return new CurrencyExchangeResponse(1, from, to, 65, environment.getProperty("local.server.port"));
     }
 }
