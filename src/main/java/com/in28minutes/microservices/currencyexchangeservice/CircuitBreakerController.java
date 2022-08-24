c6cb2ece2cfaca81044cb62bf7dd43b9bbc76b25
Max Gomes <maximiliano.pinto@klarna.com>
2022-08-24 15:40:57 +0200

    209

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,6 +1,8 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,6 +24,21 @@ public class CircuitBreakerController {
         var dummy = new RestTemplate().getForEntity("http://localhost:8080/dummy", String.class);
 
         return dummy.getBody();
+    }
+
+    @GetMapping("/sample-api-limited")
+    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+    @RateLimiter(name = "default")
+    public String sampleApiLimited () {
+        logger.info("Sample Api Limited call received");
+        return "Sample api limited";
+    }
+
+    @GetMapping("/sample-api-bulk")
+    @Bulkhead(name = "default")
+    public String sampleApiBulk () {
+        logger.info("Sample Api Limited call received");
+        return "Sample api limited";
     }
 
     public String hardcodedResponse (Exception ex) {
